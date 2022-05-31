848cdc7e1bee7b5df79feb671459c5fdf62b3ea1
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2022-06-01 03:51:47 +0500

    Commit Update

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -12,7 +12,6 @@ const ExpenseDate = (props) => {
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
       <div className="expense-date__day">{day}</div>
-      div
     </Card>
   );
 };
